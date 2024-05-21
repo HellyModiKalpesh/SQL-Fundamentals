7fa9e0fcda248b238909f72ef73036e4ddc59768
HellyModiKalpesh <105526680+HellyModiKalpesh@users.noreply.github.com>
2024-05-21 12:17:23 +0530

    added file

--- Portfolio Project- Data cleaning/Data cleaning.sql
+++ Portfolio Project- Data cleaning/Data cleaning.sql
@@ -1,9 +1,13 @@
  -- Data cleaning
  -- steps:
- -- 4.remove columns
+ -- 1.Remove dupliactes
+ -- 2.Standarize the data
+ -- 3.Null values or blank values
+ -- 4.Remove any columns or rows
+
+
  create table layoff_staging
  like layoffs;
-
 -- inser data
 insert layoff_staging select * from layoffs;
 
@@ -70,3 +74,32 @@ SET date = STR_TO_DATE(date, '%m/%d/%Y');
 alter table layoffs_staging2 modify column date  DATE;
 
 -- STEP 3 NULL VALUES AND BLANK VALUES
+
+select * from layoffs_staging2;
+
+-- here there are null values in total_laid_off and percentage_laid_Off
+
+select * from layoffs_staging2 where total_laid_off is null and percentage_laid_off is null;
+
+select  * from layoffs_staging2 where industry is null or industry='';
+-- in industry there are null values present in it so let's fix it
+
+select * from layoffs_staging2 where company='Airbnb';
+-- here in company as airbnb we get industry as travel and then another entry is blank so let's update all the blank value as travel for airbnb and for another also
+
+select * from layoffs_staging2 t1 join layoffs_staging2 t2 on t1.company=t2.company and t1.location=t2.location
+where (t1.industry is null or t1.industry ='') and t2.industry is not null;
+
+-- first update all the blank value with null values
+update layoffs_staging2 set industry = null where industry='';
+
+-- now let's replace it
+
+update layoffs_staging2 t1 join layoffs_staging2 t2 on t1.company=t2.company and t1.location=t2.location set t1.industry=t2.industry where t1.industry is null and t2.industry is not null;
+
+-- now we will delete that row where total_laid off and percentage_laid_off is null
+delete from  layoffs_staging2 where total_laid_off is null and percentage_laid_off is null;
+-- now we don't need row_num also 
+alter table layoffs_staging2 drop column row_num;
+
+select * from layoffs_staging2;
