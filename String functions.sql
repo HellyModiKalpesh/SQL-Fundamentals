385f0350d09bd97d950d61a32317755943a95556
Helly modi <190320116021.it.helly.modi@gmail.com>
2024-07-15 13:55:01 +0530

    Update String functions.sql

--- String functions.sql
+++ String functions.sql
@@ -23,6 +23,10 @@ select first_name,left(first_name,4),right(first_name,4)
 select first_name, birth_date,substring(birth_date,6,2) as birth_month from employeement_demographic;
 # here substring(birth_date,6,2) means it will start from 6 position and then take two character 
 
+# we can concat multiple row values into single value separated by comma
+ GROUP_CONCAT
+ GROUP_CONCAT(col1 separator ',')
+
 # REPLACE 
 select first_name, replace(first_name,'a','z')from employeement_demographic;
 # it will replace first_name where small a is there it will replace it into z
